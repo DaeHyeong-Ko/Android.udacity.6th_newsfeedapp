bbeea9f3091d438b60db28ee1e690a1c6d8c4c70
ko <kei986394@gmail.com>
2021-03-20 20:27:44 +0900

    This is my final project in udacity, Android NANO-DEGREE COURSE. It's News Feed app for someone using by Guardian API. SO We can all of topics in This app contains 10 Topics.

--- app/src/main/java/com/example/newsfeedapp/MainActivity.java
+++ app/src/main/java/com/example/newsfeedapp/MainActivity.java
@@ -20,14 +20,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<newsfeedapp>>{
-
-    private static final String guardianapi ="https://content.guardianapis.com/search";
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<newsfeedapp>> {
+    private static final String guardianapi = "https://content.guardianapis.com/search";
     private newsfeedappAdapter mAdapter;
     private static final int newsapp_LOADER_ID = 1;
     private TextView mEmptyStateTextView;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,11 +40,10 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         ConnectivityManager connectiVityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInformation = connectiVityManager.getActiveNetworkInfo();
 
-        if(networkInformation != null && networkInformation.isConnected()){
+        if (networkInformation != null && networkInformation.isConnected()) {
             LoaderManager loaderManager = getLoaderManager();
             loaderManager.initLoader(newsapp_LOADER_ID, null, this);
-        }
-        else{
+        } else {
             View loadingIndicator = findViewById(R.id.progress_circular);
             loadingIndicator.setVisibility(View.GONE);
             mEmptyStateTextView.setText(R.string.no_internet_connection);
@@ -68,29 +64,27 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     }
 
     @Override
-    public Loader<List<newsfeedapp>> onCreateLoader(int i, Bundle bundle){
+    public Loader<List<newsfeedapp>> onCreateLoader(int i, Bundle bundle) {
         Uri guardian_API = Uri.parse(guardianapi);
         Uri.Builder uri_Builder = guardian_API.buildUpon();
-        uri_Builder.appendQueryParameter("show-tags","contributor");
-        uri_Builder.appendQueryParameter("api-key","test");
-
+        uri_Builder.appendQueryParameter("show-tags", "contributor");
+        uri_Builder.appendQueryParameter("api-key", "test");
         return new newsfeedappLoader(this, uri_Builder.toString());
     }
 
     @Override
-    public void onLoadFinished(Loader<List<newsfeedapp>> loader, List<newsfeedapp> newsfeedapps){
+    public void onLoadFinished(Loader<List<newsfeedapp>> loader, List<newsfeedapp> newsfeedapps) {
         View progressbar = findViewById(R.id.progress_circular);
         progressbar.setVisibility(View.GONE);
         mAdapter.clear();
         mEmptyStateTextView.setText(R.string.no_news);
-        if(newsfeedapps !=null && !newsfeedapps.isEmpty()){
+        if (newsfeedapps != null && !newsfeedapps.isEmpty()) {
             mAdapter.addAll(newsfeedapps);
         }
     }
 
     @Override
-    public void onLoaderReset(Loader<List<newsfeedapp>> loader)
-    {
+    public void onLoaderReset(Loader<List<newsfeedapp>> loader) {
         mAdapter.clear();
     }
 
